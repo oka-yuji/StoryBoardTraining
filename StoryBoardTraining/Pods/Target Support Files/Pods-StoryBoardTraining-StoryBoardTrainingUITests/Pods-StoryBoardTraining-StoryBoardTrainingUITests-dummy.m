3cae7d9e5645459d0441f1aff937e41fd153cb35
岡優志 <yu.j.0513@gmail.com>
2021-05-05 16:10:04 +0900

    Swift Lintをインストールしました。

---
+++ StoryBoardTraining/Pods/Target Support Files/Pods-StoryBoardTraining-StoryBoardTrainingUITests/Pods-StoryBoardTraining-StoryBoardTrainingUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StoryBoardTraining_StoryBoardTrainingUITests : NSObject
+@end
+@implementation PodsDummy_Pods_StoryBoardTraining_StoryBoardTrainingUITests
+@end
---
+++ StoryBoardTraining/Pods/Target Support Files/Pods-StoryBoardTrainingTests/Pods-StoryBoardTrainingTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StoryBoardTrainingTests : NSObject
+@end
+@implementation PodsDummy_Pods_StoryBoardTrainingTests
+@end
