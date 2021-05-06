5c927544e2612756060d2d89f5fc811e15413f18
岡優志 <yu.j.0513@gmail.com>
2021-05-06 22:54:19 +0900

    SPMを入れる練習とボタンを押すとUIimageが変わる練習をした

--- StoryBoardTraining/StoryBoardTraining/SecondViewController.swift
+++ StoryBoardTraining/StoryBoardTraining/SecondViewController.swift
@@ -6,16 +6,22 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class SecondViewController: UIViewController {
     @IBOutlet weak var secondViewTextField: UITextField!
     @IBOutlet weak var secondViewLabel: UILabel!
     @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         actionButton.setTitle("Go!!", for: .normal)
+        
+        
     }
     @IBAction func actionButton(_ sender: Any) {
         secondViewLabel.text = secondViewTextField.text
+        imageView.image = UIImage(named: "\(YumemiWeather.fetchWeather())")
     }
 }
--- StoryBoardTraining/StoryBoardTraining/ThirdViewController.swift
+++ StoryBoardTraining/StoryBoardTraining/ThirdViewController.swift
@@ -9,20 +9,11 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    @IBOutlet weak var weatherImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        weatherImageView.image = UIImage(named: "sunny")
+        
     }
-    */
-
 }
